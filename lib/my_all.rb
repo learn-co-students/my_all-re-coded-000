e374ae340179acada3bd73a3510b6f090833007a
zainab amjad <z_amjad@yahoo.com>
2017-01-08 07:17:13 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  res = []
+  while i < collection.length
+  res << yield(collection[i])
+    i = i + 1
+  end
 
+  if res.include?(false)
+    false
+  else
+    true
+  end
 end
